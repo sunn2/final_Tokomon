91aad61475cd52b6933604bbfb6baee96734aac8
sunn2 <sunnyda23@karts.ac.kr>
2021-06-12 16:09:16 +0900

    Update sketch.js
    21.06.12 면담 후

--- sketch.js
+++ sketch.js
@@ -7,20 +7,19 @@ let closedMouth;
 let iLoveOrange;
 let orange;
 let mushroom;
-let Tmad1;
-let Tmad2;
+let Tmad;
+let full;
+let hungry;
 //이미지
 
 let myTokomon;
+let foods = [];
 
-let xO = [];
-let yO = [];
-let xM = [];
-let yM = [];
-
+let stage = 0; //0;시작 1;게임 2;엔딩
+let mouth = false;
 
 
-function preload(){
+function preload() {
     egg = loadImage('토코몬캡쳐/알.png');
     hatch = loadImage('토코몬캡쳐/깨진알.png');
     poyomon = loadImage('토코몬캡쳐/포요몬.png');
@@ -30,8 +29,9 @@ function preload(){
     iLoveOrange = loadImage('토코몬캡쳐/오렌지좋아.PNG');
     orange = loadImage('토코몬캡쳐/오렌지.PNG');
     mushroom = loadImage('토코몬캡쳐/독버섯.PNG');
-    Tmad1 = loadImage('토코몬캡쳐/화남1.PNG');
-    Tmad2 = loadImage('토코몬캡쳐/화남2.PNG');
+    Tmad = loadImage('토코몬캡쳐/화남3.jpeg');
+    full = loadImage('토코몬캡쳐/배부른토코몬.PNG');
+    hungry = loadImage('토코몬캡쳐/배고픈토코몬.PNG');
 }
 
 function setup() {
@@ -40,66 +40,110 @@ function setup() {
     imageMode(CENTER);
 
     myTokomon = new Tokomon;
+    for (let i = 0; i < 10; i++) {
+        foods[i] = new Food(floor(random(2)));
+    }
 }
 
 function draw() {
     background(255);
-//     image(egg, windowWidth/2, windowHeight/2);
-//     textSize(20);
-//     text('start', windowWidth/2-16, windowHeight/2 + 100); //시작화면
+    let d = dist(mouseX, mouseY, windowWidth / 2, windowHeight / 2);
 
-//     let d = dist(mouseX, mouseY, windowWidth/2, windowHeight/2);
-//     if(d<100){
-//         image(hatch, windowWidth/2, windowHeight/2);
-//         if(mouseIsPressed){
-//             image(poyomon, windowWidth/2, windowHeight/2, 150, 150); //스타트 눌렀을 때
-//         }
-//     }
-// }
-            myTokomon.display();
-            myTokomon.move();
+    if (stage == 0) {
+        image(egg, windowWidth / 2, windowHeight / 2);
+        textSize(20);
+        text('start', windowWidth / 2 - 16, windowHeight / 2 + 100); //시작화면
+
+        if (d < 100) {
+            image(hatch, windowWidth / 2, windowHeight / 2);
+            if (mouseIsPressed) {
+                image(poyomon, windowWidth / 2, windowHeight / 2, 150, 150); //스타트 눌렀을 때
+                stage = 1;
+            }
+        }
+    } else if (stage == 1) {
+        myTokomon.display();
+        
+        for (let i = 0; i < 10; i++) {
+            foods[i].display();
+            foods[i].move();
+        }
+        // if(오렌지 다 먹으면){
+        //     stage = 2;
+        // }else if (시간 안에 오렌지 다 못 먹으면){
+        //     stage = 3;
+        // }
+    } else if(stage == 2){
+        image(full, windowWidth/2, windowHeight/2, windowWidth, windowHeight);
+    } else if(stage == 3){
+        image(hungry, windowWidth/2, windowHeight/2, windowWidth, windowHeight);
+    }
 }
 
 
-//게임 시작하기 
-class Tokomon{
+// }
 
-    constructor(){
-        for(let i=0; i<10; i++){
-            xO[i] = random(100, width-100);
-            yO[i] = random(100, height-100);
-            xM[i] = random(100, width-100);
-            yM[i] = random(100, height-100);
 
-            this.dO = dist(mouseX, mouseY, xO[i], yO[i]);
-            this.dM = dist(mouseX, mouseY, xM[i], yM[i]);
-            this.dT = dist(mouseX, mouseY, windowWidth/2, windowHeight/2);
+//게임 시작하기
+class Tokomon {
+    constructor() {
+        }
+        
+        display() {
+            background(255);
+            if (!mouth) {
+                image(closedMouth, windowWidth / 2, windowHeight / 2, 200, 200);
+            } else {
+                image(openedMouth, windowWidth / 2, windowHeight / 2, 210, 250);
+            }
+        }
+}
+
+class Food {
+    constructor(_kind) {
+        this.kind = _kind;
+        this.x = random(100, width - 100);
+        this.y = random(100, height - 100);
+        this.d;
+        this.taken = false;
+        // this.takenM = false;
+        // this.takenO = false;
+
+        //0 -> m, 1 -> O
+        // if (this.kind == 0) {
+        //     image(Tmad, windowWidth/2,windowHeight/2);
+        // }
+    }
+    
+    display() {
+        if (!this.taken) {
+            if (this.kind == 0) {
+                image(mushroom, this.x, this.y, 100, 100);
+            } else if(this.kind == 1){
+                image(orange, this.x, this.y, 100, 100);
+            }
         }
     }
     
-    display(){
-        background(255);
-        image(closedMouth, windowWidth/2, windowHeight/2, 200, 200);
-
-        for(let i=0; i<10; i++){
-            image(orange, xO[i], yO[i], 100, 100);
-            image(mushroom, xM[i], yM[i], 100, 100);
-        }
-    }
-
-    move(){
-
-        if(this.dO < 50){
-            if(mouseIsPressed){
-                xO[i] = mouseX;
-                yO[i] = mouseY;
-                if(this.dT < 200){
-                    image(openedMouth, windowWidth/2, windowHeight/2, 210, 250);
-
-                }
+    move() {
+        this.d = dist(mouseX, mouseY, this.x, this.y);
+        this.dT = dist(mouseX, mouseY, width / 2, height / 2);
+        if (this.d < 40) {
+            if (mouseIsPressed) {
+                this.x = mouseX;
+                this.y = mouseY;
+                    if (!this.taken && this.dT < 50) {
+                        mouth = true;
+                        this.taken = true;
+                        if(this.kind == 0){
+                            image(Tmad, windowWidth/2, windowHeight/2); //버섯 먹이면
+                        }else if(this.kind == 1){
+                            image(iLoveOrange, windowWidth/2, windowHeight/2, 200, 200); //오렌지 먹이면
+                        }
+                    } else {
+                        mouth = false;
+                    }
             }
         }
     }
 }
-
-
